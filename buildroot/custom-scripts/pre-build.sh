24026f4cc1ecd26a155e69101adff4cbeb9e8c71
Davi-K-Silva <davi.kds.01@gmail.com>
2022-08-30 09:26:40 -0300

    Server app and crosscompilation script added

--- buildroot/custom-scripts/pre-build.sh
+++ buildroot/custom-scripts/pre-build.sh
@@ -2,3 +2,6 @@
 
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+
+$BASE_DIR/host/bin/i686-buildroot-linux-uclibc-gcc $BASE_DIR/../../programs/simple_http_server_multiclient.c -o $BASE_DIR/target/usr/bin/simple_http_server_multiclient
+chmod +x $BASE_DIR/target/usr/bin/simple_http_server_multiclient
