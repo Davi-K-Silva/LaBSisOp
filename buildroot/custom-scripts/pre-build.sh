1d2e6d03aa3da65b8cf7b0bf26b08cc5fe215b7f
Davi-K-Silva <davi.kds.01@gmail.com>
2022-09-06 08:14:58 -0300

    Server app upgraded

--- buildroot/custom-scripts/pre-build.sh
+++ buildroot/custom-scripts/pre-build.sh
@@ -5,3 +5,6 @@ chmod +x $BASE_DIR/target/etc/init.d/S41network-config
 
 $BASE_DIR/host/bin/i686-buildroot-linux-uclibc-gcc $BASE_DIR/../../programs/simple_http_server_multiclient.c -o $BASE_DIR/target/usr/bin/simple_http_server_multiclient
 chmod +x $BASE_DIR/target/usr/bin/simple_http_server_multiclient
+
+cp $BASE_DIR/../../programs/page.html $BASE_DIR/target/usr/bin
+chmod +xr $BASE_DIR/target/usr/bin/page.html
