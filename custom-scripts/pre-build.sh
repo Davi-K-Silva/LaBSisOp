ba514a6ac6b10ca9d0a003ab16e5925889720620
Davi-K-Silva <davi.kds.01@gmail.com>
2022-09-08 08:34:16 -0300

    Kernel dev enviroment

---
+++ custom-scripts/pre-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
+chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+
+$BASE_DIR/host/bin/i686-buildroot-linux-uclibc-gcc $BASE_DIR/../../programs/simple_http_server_multiclient.c -o $BASE_DIR/target/usr/bin/simple_http_server_multiclient
+chmod +x $BASE_DIR/target/usr/bin/simple_http_server_multiclient
+
+cp $BASE_DIR/../../programs/page.html $BASE_DIR/target/usr/bin
+chmod +xr $BASE_DIR/target/usr/bin/page.html
